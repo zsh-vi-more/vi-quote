4083e05dd2b8aab2ee9249389bfeede3fd2880c0
gammafn <GammaFunction@vivaldi.net>
2020-02-08 19:54:00 -0600

    move vi-quote to functions

--- vi-quote.zsh
+++ vi-quote.zsh
@@ -3,8 +3,8 @@
 # ref: zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
-[[ $zsh_loaded_plugins[-1] != */vi-quote && -z $fpath[(r)${0:h}] ]] &&
-	fpath+=("${0:h}")
+[[ $zsh_loaded_plugins[-1] != */vi-quote && -z $fpath[(r)${0:h}/functions] ]] &&
+	fpath+=("${0:h}/functions")
 # }}}
 # toggle quotes around a motion
 autoload -Uz vi-quote
